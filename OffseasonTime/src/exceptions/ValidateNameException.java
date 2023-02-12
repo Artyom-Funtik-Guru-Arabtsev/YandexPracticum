39d4c378d86a46908ed643bbec43e11682c5afeb
Artyom 'Funtik Guru' Arabtsev <artyomarabcev@yandex.ru>
2023-02-12 23:26:38 +0300

    ttt

---
+++ OffseasonTime/src/exceptions/ValidateNameException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ValidateNameException extends ValidateException {
+    public ValidateNameException(final String value){
+        super(value);
+    }
+// допишите код класса
+}
