b9a2cdb391f474a0964ce3d0fd8b0c78b7cd7e71
Artyom 'Funtik Guru' Arabtsev <artyomarabcev@yandex.ru>
2023-02-14 02:09:05 +0300

    яя

--- OffseasonTime/src/Practicum.java
+++ OffseasonTime/src/Practicum.java
@@ -1,104 +1,34 @@
-import exceptions.ValidateException;
-import exceptions.ValidateNameException;
-import exceptions.ValidatePasswordException;
-import storage.PasswordMemoryStorage;
-import storage.PasswordStorage;
-import validators.NameValidator;
-import validators.PasswordLengthValidator;
-import validators.PasswordStrengthValidator;
-import validators.Validator;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-class Practicum {
+public class Practicum {
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final List<Validator> passwordValidators = List.of(
-            new PasswordLengthValidator(5), new PasswordStrengthValidator()
-    );
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> frequencyMap = new HashMap<>();
 
-    private static final List<Validator> nameValidators = List.of(
-            new NameValidator()
-    ); // поработайте со списком
+        FileReader reader = new FileReader("result.txt");
+        BufferedReader br = new BufferedReader(reader);
 
-    public static void main(String[] args) throws ValidateException {
-        loop();
-    }
-
-    public static void loop() throws ValidateException {
-        while (true) {
-            final String action = getAction();
-            if ("1".equals(action)) {
-                addUser();
-            } else if ("2".equals(action)) {
-                showUserPassword();
-            } else {
-                break;
+        while (br.ready()){
+            String voice = br.readLine();
+            if (frequencyMap.containsKey(voice)){
+                Integer count = frequencyMap.get(voice) + 1;
+                frequencyMap.put(voice,count);
+            }
+            else {
+                frequencyMap.put(voice,1);
             }
         }
-    }
 
-    private static void checkValidatorRules(
-            final List<Validator> validators, final String value
-    ) throws ValidateException {
-        for (Validator validator : validators) {
-            validator.validate(value);
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()){
+            System.out.printf("%s: %d\n",entry.getKey(),entry.getValue());
         }
-    }
+        // читайте файл построчно и сразу обновляйте frequencyMap.
 
-    private static void addUser() throws ValidateException{
-        final PasswordStorage storage = new PasswordMemoryStorage();
-        // добавьте отлов исключений ValidateNameException и ValidatePasswordException
-        // закройте хранилище
-        try {
-            storage.open();
-            System.out.println("Введите имя пользователя => ");
-            final String name = scanner.nextLine();
-            checkValidatorRules(nameValidators, name);
-            System.out.println("Введите пароль пользователя => ");
-            final String password = scanner.nextLine();
-            checkValidatorRules(passwordValidators, password);
-            storage.store(name, password);
-        } catch (ValidateNameException e ) {
-            System.out.println("Ошибка валидации имени: " + e.getMessage());
-        }catch (ValidatePasswordException e){
-            System.out.println("Ошибка валидаци пароля: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Ошибка работы с хранилищем: " + e.getMessage());
-        } finally {
-            storage.close();
-        }
-    }
+        // выведите результат в формате "<буква>: <количество>".
 
-    private static void showUserPassword() throws ValidateException {
-        final PasswordStorage storage = new PasswordMemoryStorage();
-        // добавьте отлов исключения ValidateNameException
-        // закройте хранилище
-        try {
-            storage.open();
-            System.out.println("Введите имя пользователя => ");
-            final String name = scanner.nextLine();
-            checkValidatorRules(nameValidators, name);
-            final String password = storage.get(name);
-            System.out.println(String.format("Пароль пользователя %s = %s", name, password));
-        } catch (ValidateNameException e) {
-            System.out.println("Ошибка валидации имени: " + e.getMessage());
-        } catch (ValidatePasswordException e) {
-            System.out.println("Ошибка валидации пароля: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Ошибка работы с хранилищем: " + e.getMessage());
-        } finally {
-            storage.close();
-        }
-    }
-
-    private static String getAction() {
-        System.out.println("1 - добавить пользователя с паролем");
-        System.out.println("2 - отобразить пароль пользователя");
-        System.out.println("другие символы - выход");
-        System.out.println("Выберите действие => ");
-        return scanner.nextLine();
     }
 }
