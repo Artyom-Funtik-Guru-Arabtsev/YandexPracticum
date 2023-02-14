d85f9ab5cd72207867c8ff0ff91a7fe04443036b
Artyom 'Funtik Guru' Arabtsev <116725157+Artyom-Funtik-Guru-Arabtsev@users.noreply.github.com>
2023-02-14 21:46:46 +0300

    яя

--- OffseasonTime/src/Practicum.java
+++ OffseasonTime/src/Practicum.java
@@ -1,34 +1,68 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class Practicum {
 
-    public static void main(String[] args) throws IOException {
-        Map<String, Integer> frequencyMap = new HashMap<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        FileReader reader = new FileReader("result.txt");
-        BufferedReader br = new BufferedReader(reader);
+        System.out.println("Введите количество участников: ");
+        int playersNumber = scanner.nextInt();
 
-        while (br.ready()){
-            String voice = br.readLine();
-            if (frequencyMap.containsKey(voice)){
-                Integer count = frequencyMap.get(voice) + 1;
-                frequencyMap.put(voice,count);
-            }
-            else {
-                frequencyMap.put(voice,1);
-            }
+        List<String> words = readWordsFromFile("words.txt");
+        if (words.size() < playersNumber) {
+            System.out.println("Недостаточно слов в файле. Добавьте слова и обновите файл.");
+            return;
         }
 
-        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()){
-            System.out.printf("%s: %d\n",entry.getKey(),entry.getValue());
+        // если слов меньше, чем участников, то выведите сообщение:
+        // "Недостаточно слов в файле. Добавьте слова и обновите файл."
+        // и завершите выполнение программы
+        Collections.shuffle(words);
+        // воспользуйтесь статическим методом Collections.shuffle(List<?> list),
+        // чтобы поменять порядок слов случайным образом
+
+        int wordsNumber = words.size() / playersNumber;
+
+        for (int i = 0; i < playersNumber; i++) {
+            String filename = String.format("player%s.txt", i + 1);
+            List<String> subList = words.subList(i * wordsNumber, (i + 1) * wordsNumber);
+
+            writeListToFile(subList, filename);
         }
-        // читайте файл построчно и сразу обновляйте frequencyMap.
 
-        // выведите результат в формате "<буква>: <количество>".
+        System.out.println("Карточки готовы!");
+    }
 
+    private static List<String> readWordsFromFile(String filename) {
+        List<String> listOut = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename, StandardCharsets.UTF_8))) {
+            while (bufferedReader.ready()) {
+                listOut.add(bufferedReader.readLine());
+            }
+            return listOut;
+        } catch (IOException exception) {
+            System.out.println("Произошла ошибка во время чтения файла");
+            return listOut;
+        }
+        // добавьте построчное чтение из файла с помощью BufferedReader
+        // в случае ошибки выведите сообщение: "Произошла ошибка во время чтения файла."
+    }
+
+    private static void writeListToFile(List<String> list, String filename) {
+        try (FileWriter fileWriter = new FileWriter(filename, StandardCharsets.UTF_8)){
+            for (String words : list) {
+                fileWriter.write(words);
+            }
+        }
+        catch (IOException exception){
+            System.out.println("Произошла ошибка во время записи файла.");
+        }
+        // добавьте запись слов в файл с помощью FileWriter
+        // в случае ошибки выведите сообщение: "Произошла ошибка во время записи файла."
     }
 }
